a794f34a74a097b058a191c4adaf5a2e3d86f933
Elena S <safon4ik@gmail.com>
2018-05-18 04:21:28 +0000

    indrx and direction pages

---
+++ js/myScript.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    
+    $('#btn1').click(function(){
+        $('#box1').toggle(3000);
+    })
+    
+    $('#btn2').click(function(){
+        $('#box2').toggle('slow');
+    })
+})

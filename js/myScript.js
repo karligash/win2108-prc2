b70be3b66c50bde743ef4dad629ca44d95e54acf
Elena S <safon4ik@gmail.com>
2018-05-22 17:37:48 +0000

    buttons

--- js/myScript.js
+++ js/myScript.js
@@ -2,10 +2,19 @@
 $(document).ready(function(){
     
     $('#btn1').click(function(){
-        $('#box1').toggle(3000);
+        $('#box1').toggle('slow');
     })
     
     $('#btn2').click(function(){
         $('#box2').toggle('slow');
     })
+    
+    $('#clickme').click(function(){
+        $('#img2').animate({
+           left: '250px',
+           height: '400 px',
+           width: '400px'
+       } );
+    });
 })
+
